f9eac5d6a90a61cef957078ac695067442abf9d1
이준하 <>
2020-03-06 19:23:04 +0900

    #2667 - solved

--- solved/2667/2667.py
+++ solved/2667/2667.py
@@ -1,25 +1,27 @@
 n = int(input())
 
-graph = [[0]*(n+1) for _ in range]
-
-
 house = []
-for _ in range(int(input())):
-    house += [list(map(int, list(input())))]
+for _ in range(n):
+    house.append(list(map(int, list(input()))))
 
-
-print(graph)
-
-
-
-
-
-def dfs(current, graph, visit):
-    visit.append(current)
-
-    for next in range(len(graph[current])):
-        if graph[current][next] and next not in visit:
-            visit = dfs(next,graph,visit)
+def dfs(x,y,graph,visit,n):
+    if x>=0 and y>=0 and x<n and y <n and graph[x][y] and [x,y] not in visit:
+        visit.append([x,y])
+        graph[x][y] = 0
+        if x+1 <= n:
+            visit = dfs(x+1,y,graph,visit,n) 
+        if y+1 <= n:
+            visit = dfs(x,y+1,graph,visit,n) 
+        visit = dfs(x-1,y,graph,visit,n) 
+        visit = dfs(x,y-1,graph,visit,n)
     return visit
 
 answer = []
+for i in range(n):
+    for k in range(n):
+        if house[i][k]:
+            answer.append(len(dfs(i,k,house,[],n)))
+answer.sort()
+print(len(answer))
+for a in answer:
+    print(a)
--- solved/2667/README.md
+++ solved/2667/README.md
@@ -2,10 +2,19 @@
 
 ### 풀이
 
-* 
+* DFS // 문제에 맞는 알고리즘을 만들어줄것!
 
 ```Python
-
-
+def dfs(x,y,graph,visit,n):
+    if x>=0 and y>=0 and x<n and y <n and graph[x][y] and [x,y] not in visit:
+        visit.append([x,y])
+        graph[x][y] = 0
+        if x+1 <= n:
+            visit = dfs(x+1,y,graph,visit,n) 
+        if y+1 <= n:
+            visit = dfs(x,y+1,graph,visit,n) 
+        visit = dfs(x-1,y,graph,visit,n) 
+        visit = dfs(x,y-1,graph,visit,n)
+    return visit
 ```
 
