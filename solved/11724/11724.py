9c27418b13890b47e5292e5b9a935d2898eb05a0
이준하 <>
2020-03-24 02:50:10 +0900

    #11724 - solved

--- solved/11724/11724.py
+++ solved/11724/11724.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 n,m = map(int, input().split())
+
 graph = [[0]*(n+1) for _ in range(n+1)]
 
 for _ in range(m):
@@ -8,21 +9,21 @@ for _ in range(m):
     graph[link[0]][link[1]] = 1
     graph[link[1]][link[0]] = 1
 
-def dfs(current,graph,visit):
-
-    visit.append(current)
+def dfs(current):
+    global graph, visit
+    visit[current] = 1
 
     for next in range(len(graph[current])):
-        if graph[current][next] and next not in visit:
-            visit = dfs(next, graph, visit)
+        if graph[current][next] and visit[next] == 0:
+            visit = dfs(next)
             
     return visit
 
 ans = []
-for i in range(n):
-    temp = dfs(i,graph,[])
-    temp.sort()
+for i in range(1,n+1):
+    visit = [0 for _ in range(n+1)]
+    temp = dfs(i)
     if temp not in ans:
         ans.append(temp)
 
-print(len(ans)-1)
+print(len(ans))
--- solved/11724/README.md
+++ solved/11724/README.md
@@ -2,10 +2,32 @@
 
 ### 풀이
 
-* 
+* 더욱 빠른 DFS 만들기
 
 ```Python
+### 내가 자주 사용하던 DFS
+
+def dfs(current, visit, graph):
+	visit.append(current)
+
+	for next in range(len(graph[current])):
+		if graph[current][next] and next not in visit:
+			visit = dfs(next, visit, graph)
+
+	return visit
 
 
+
+### 속도를 더욱 향상 시켜준 DFS
+def dfs(current):
+    global graph, visit
+    visit[current] = 1
+
+    for next in range(len(graph[current])):
+        if graph[current][next] and visit[next] == 0:
+            visit = dfs(next)
+            
+    return visit
 ```
 
+* visit이라는 [0 for _ in range(n+1)]이라는 배열을 만들어놓고, 방문하였으면 visit[current] = 1로 만들어 준다.
