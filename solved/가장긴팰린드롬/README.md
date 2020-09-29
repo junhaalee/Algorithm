e14c20610f38e2510b2849438839e9ccb3cfafdb
이준하 <>
2020-09-29 23:53:36 +0900

    #가장 긴 팰린드롬 - solved

---
+++ solved/가장긴팰린드롬/README.md
@@ -0,0 +1,14 @@
+### 프로그래머스 - [가장 긴 팰린드롬](https://programmers.co.kr/learn/courses/30/lessons/12904)
+
+### 풀이
+
+* 
+
+```Python
+
+a = [1,2,3,4]
+b = a[::-1]
+b >>> [4,3,2,1]
+
+```
+
---
+++ solved/가장긴팰린드롬/가장긴팰린드롬.py
@@ -0,0 +1,9 @@
+def solution(s):
+    
+    answer = 0
+
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            if s[i:j+1] == s[i:j+1][::-1]:
+                answer = max([answer,j-i+1])
+    return answer
